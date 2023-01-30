67ce97a4dd759a68c0139c7b409af2ca1ea6f4ab
rlawlsdn263 <rlawlsdn263@naver.com>
2023-01-30 17:44:43 +0900

    ㅁㄴㅇ

---
+++ client/js/main.js
@@ -0,0 +1,13 @@
+const swiper = new Swiper(".mySwiper", {
+  autoplay: true,
+  loop: true,
+  speed: 1000,
+  pagination: {
+    el: ".swiper-pagination",
+    type: "fraction",
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
---
+++ client/js/swiper/swiper.js
@@ -0,0 +1,7 @@
+const swiper = new Swiper(".swiper", {
+  autoplay: true,
+  loop: true,
+  speed: 2000,
+});
+
+swiper.autoplay();
